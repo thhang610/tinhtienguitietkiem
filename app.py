5a64a1550cbad4bb8eaba4ac3074fb84e846e578
thhang610 <tranphamthuyhang610@gmail.com>
2026-06-16 16:57:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.image("logo.jpg")
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_ĐỀ TÀI 4_NGUYỄN NGỌC KIỀU VY")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_ĐỀ TÀI 1_PHẠM NGỌC TRÂM")
 
 # Nhập dữ liệu
 C = st.number_input(
