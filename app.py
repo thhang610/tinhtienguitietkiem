2d37621c0f01888058f7154d23e3fbe724eb2594
thhang610 <tranphamthuyhang610@gmail.com>
2026-06-16 16:42:58 +0700

    Update title and add logo image to app

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-
+st.image("logo.jpg")
 # Tiêu đề ứng dụng
-st.title("💰 Ứng dụng tính tiền gửi tiết kiệm")
+st.title("💰 Ứng dụng tính tiền gửi tiết kiệm_ĐỀ TÀI 1_TRẦN PHẠM THÚY HẰNG")
 
 # Nhập dữ liệu
 C = st.number_input(
